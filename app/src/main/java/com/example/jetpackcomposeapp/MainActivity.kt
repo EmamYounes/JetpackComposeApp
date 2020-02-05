7f65ee323a8b907fcbafa7d8880b30ee815ae27c
Emam <emamAhmed36@gmail.com>
2020-02-05 12:54:03 +0200

    add Preview function

--- app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.tooling.preview.Preview
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,13 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setContent {
-//            Text("Hello World")
-            Greeting("Android")
+
+            //            Text("Hello World")
+           Greeting("Android")
+//            PreviewGreeting()
+
         }
     }
 
     @Composable
     fun Greeting(name: String) {
         Text("Hello $name")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewGreeting() {
+        Greeting("Android")
+    }
+    @Preview
+    @Composable
+    fun DefaultPreview() {
+        Greeting("Android")
     }
 }
