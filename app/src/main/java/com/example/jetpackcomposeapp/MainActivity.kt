242a706abbee8d7d542d291b6fc6526371eb5322
Emam <emamAhmed36@gmail.com>
2020-02-06 10:44:37 +0200

    add NewsStory

--- app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.jetpackcomposeapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.unaryPlus
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
+import androidx.ui.foundation.DrawImage
+import androidx.ui.layout.*
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +18,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setContent {
+            NewsStory()
+        }
+    }
 
-            //            Text("Hello World")
-           Greeting("Android")
-//            PreviewGreeting()
+    @Composable
+    fun NewsStory() {
+        val image = +imageResource(R.drawable.background)
+        Column(
+            modifier = Spacing(16.dp)
+        ) {
+            Container(modifier = Spacing(90.dp)) {
+                DrawImage(image)
+            }
+
+            HeightSpacer(16.dp)
+
+            Text("A day in Shark Fin Cove")
+            Text("Davenport, California")
+            Text("December 2018")
+        }
+    }
+
+    @Preview
+    @Composable
+    fun DefaultPreview() {
+        NewsStory()
+    }
+
+
+    // lesson one
+/*    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setContent {
+
+            Text("Hello World")
+            Greeting("Android")
+            PreviewGreeting()
 
         }
     }
@@ -32,9 +70,10 @@ class MainActivity : AppCompatActivity() {
     fun PreviewGreeting() {
         Greeting("Android")
     }
+
     @Preview
     @Composable
     fun DefaultPreview() {
         Greeting("Android")
-    }
+    }*/
 }
